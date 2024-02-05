340c9283dca512fa33b7f5ff178ab18e04a4ed07
Ninejar <125640762+Ninejar@users.noreply.github.com>
2024-02-05 12:55:27 +0000

    Added hello there stub(?) and express.js, nodemon & ejs

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+
+// set "public" as the directory for default pages
+app.use(express.static("public"));
+
+// allow ejs to be used
+app.set("view engine", "ejs");
+
+// set port to environmental variable or 3000
+const PORT = process.env.PORT || 3000;
+// listen to a server on the defined port and consolé-dot-log when it's running
+app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
+
+// serve the homepage when no additional path is specified, and print the user's username when opening the page
+let username = "Chris"
+app.get("/", (req,res) => {
+    res.render("index", {name:username});
+});
