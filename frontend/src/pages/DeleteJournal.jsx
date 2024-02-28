3d4774cd6f7e4d987a9d410cb790f34fc8f1fb20
Christopher Ngo <christopherngo@ChristophersMBP.lan>
2024-02-28 20:46:16 +0100

    delete journal functionality

--- frontend/src/pages/DeleteJournal.jsx
+++ frontend/src/pages/DeleteJournal.jsx
@@ -1,8 +1,44 @@
-import React from 'react'
+import React, {useState} from 'react'
+import BackButton from '../components/BackButton'
+import Loading from '../components/Loading'
+import Navbar from '../components/Navbar'
+import axios from 'axios'
+import { useNavigate, useParams } from 'react-router-dom'
 
 const DeleteJournal = () => {
+  const [loading, setLoading] = useState(false)
+  const navigate = useNavigate()
+  const {id} = useParams()
+  const handleDeleteJournal = () => {
+    setLoading(true)
+    axios
+      .delete(`http://localhost:1814/journals/${id}`)
+      .then(() => {
+        setLoading(false)
+        navigate('/journals/list')
+      })
+      .catch ((error) => {
+        setLoading(false)
+        alert('An error happened. Please check console')
+        console.log(error)
+      })
+  }
   return (
-    <div>DeleteJournal</div>
+    <div className='app'>
+      <Navbar />
+
+      <div>
+        <BackButton destination='/journals/list' />
+        <h1>Delete journal</h1>
+        {loading ? <Loading /> : ''}
+        <h3>Are you sure you want to delete this journal?</h3>
+        <button onClick={handleDeleteJournal}>
+          Yes, delete
+        </button>
+
+      </div>
+      
+    </div>
   )
 }
 
