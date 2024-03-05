2cc595962574431aecd0baaa1b3fd82ca1db8184
Ninejar <olansk@ntnu.no>
2024-03-05 15:34:59 +0100

    Frontendn't :(

--- sh/cp.compose.yaml.sh
+++ sh/cp.compose.yaml.sh
@@ -8,34 +8,38 @@ WD="/home/team1/projectfolder"
 
 # checking if another url has been given
 if [[ -z "$1" ]]; then
-	echo "a REPO_URL hasn't been supplied - using the defaul one: ${REPO_URL}"
+    echo "a REPO_URL hasn't been supplied - using the defaul one: ${REPO_URL}"
 else
-	REPO_URL="$1"
+    REPO_URL="$1"
 fi
 
 # silent defaults
-COMPOSE_FNAME="${2:-remote.compose.yaml}"
+COMPOSE_FNAME="${2:-docker-compose.yaml}"
 
 # get the rest of params as an array of env files
 if [[  $# -lt 3  ]]; then
-#	ENV_FNAME=(".env" ".mongo.env")
-	ENV_FNAME=()
+#   ENV_FNAME=(".env" ".mongo.env")
+    ENV_FNAME=()
 else
-	echo "shifting"
-	shift
-	shift
-	ENV_FNAME=("$@")
+    echo "shifting"
+    shift
+    shift
+    ENV_FNAME=("$@")
 fi
 
 curl -sS "${REPO_URL}/${COMPOSE_FNAME}" |
-	sed '/build: ./d' |
-	sed 's|image: yt-app-comp:0.2|image: aliakseix/idg2671:latest|' > "${WD}/compose.yaml"
+    sed '/build/d' |
+    sed '/context: ./d' |
+    sed '/dockerfile: /d' |
+    sed 's|image: mongo:latest|image: ninejar/webproject-mongo:latest|' |
+    sed 's|container_name: backend|image: ninejar/webproject-backend:latest|' |
+    sed 's|container_name: frontend|image: ninejar/webproject-frontend:latest|' > "${WD}/compose.yaml"
 for envF in "${ENV_FNAME[@]}"; do
-	# echo "$envF"
-	curl -sS "${REPO_URL}/${envF}" > "${WD}/${envF}"
+    # echo "$envF"
+    curl -sS "${REPO_URL}/${envF}" > "${WD}/${envF}"
 done
 
-echo "Donwloaded compose.yaml"
+echo "Downloaded compose.yaml"
 
 set +ex
 
