d40999573301a641125078ba959cc9de68d232c3
jorgennonstad <jorgenon@stud.ntnu.no>
2024-03-12 14:04:34 +0100

    fix body syle

--- frontend/src/components/RoleButtons/RoleButtons.jsx
+++ frontend/src/components/RoleButtons/RoleButtons.jsx
@@ -9,7 +9,7 @@ const RoleButtons = () => {
           <input type="radio" id="student" name="role" value="student" />
           <label htmlFor="student" className="roleLabel">
             <img
-              src="https://static.thenounproject.com/png/556113-200.png"
+              src="https://static.thenounproject.com/png/556113-200.png" // CHANGE TO AN IMAGE FILE!!!!! IMPORTANT
               alt="Student Icon"
               className="roleIcon"
             />
@@ -20,7 +20,7 @@ const RoleButtons = () => {
           <input type="radio" id="teacher" name="role" value="teacher" />
           <label htmlFor="teacher" className="roleLabel">
             <img
-              src="https://icons.veryicon.com/png/o/business/educational-administration-related/teacher-11.png"
+              src="https://icons.veryicon.com/png/o/business/educational-administration-related/teacher-11.png"  // CHANGE TO AN IMAGE FILE!!!!! IMPORTANT
               alt="Teacher Icon"
               className="roleIcon"
             />
