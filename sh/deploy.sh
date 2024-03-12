ae8bfc8ee114d7a532b5612f134188137b553e5d
Ninejar <olansk@ntnu.no>
2024-03-12 15:08:46 +0100

    pls work Docker I'm so tired I stg I will cry a lot :(

--- sh/deploy.sh
+++ sh/deploy.sh
@@ -2,15 +2,9 @@
 
 set -e
 
-DEFAULT_WD="/home/team1/projectfolder"
-COMPOSE_FILE="docker-compose.yaml"
+wd="$(cd "$(dirname "$0")" && pwd)"
 
-wd="${WD:-$DEFAULT_WD}"
-
-cd "$wd"
-
-docker compose -f "${wd}/${COMPOSE_FILE}" down
-docker compose -f "${wd}/${COMPOSE_FILE}" pull
-docker compose -f "${wd}/${COMPOSE_FILE}" up -d
+bash "${wd}/cp.compose.yaml.sh"
+bash "${wd}/run.docker.sh"
 
 set +e
