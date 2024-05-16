48efc02e69a62a5d02872e4d65c1f53ca362fad6
Solveig Ranøien Engen <solvre@stud.ntnu.no>
2024-05-16 16:16:34 +0200

    stylinggg

--- frontend/src/pages/WelcomePage.jsx
+++ frontend/src/pages/WelcomePage.jsx
@@ -6,28 +6,25 @@ import '../components/WelcomePage/WelcomePage.css';
 const WelcomePage = () => {
   return (
     <>
-      <h1 class="welcomeTitle">Welcome</h1>
+      <blockquote>
+        <p class="welcomeFunctionTitle introduction">Empower Students, </p>
+        <p class="welcomeAboutTitle introduction">Engage And Reflect, </p>
+        <p class="welcomeSusTitle introduction">Sustainable Future For All </p>
+      </blockquote>
       <div class="welcome-content">
         <div class="text">
-          <div class="welcomeSus">
+          <div class="welcomeSus blockOfText">
             <h2 class="welcome-subTitle">About sustainability:</h2>
             <p class="welcomeText">The United Nations has established 17 Sustainable Development Goals to address global challenges and achieve a better and more sustainable future for all by 2030. These goals cover a wide range of issues covering the most important topics. Each goal is interconnected, and the aim is to leave no one behind in the journey towards sustainable development.</p>
           </div>
-          <div class="welcomeAbout">
+          <div class="welcomeAbout blockOfText" >
             <h2 class="welcome-subTitle">About the diary's purpose:</h2>
             <p class="welcomeText">The Digital Sustainability Diary is a tool designed to help students engage with and reflect on the UN's sustainability goals. It allows students to journal about their daily actions and thoughts related to sustainability, helping them explore how their activities align with the sustainability development goals and consider ways to contribute to a more sustainable world. This reflective practice promotes personal growth and a deeper understanding of sustainability over time.</p>
           </div>
-          <div class="welcomeFunction">
-            <h2 class="welcome-subTitle">About the diary's functions:</h2>
-            <p class="welcomeText">The diary also fosters a supportive learning environment through teacher-student interaction. Students can choose to share their reflections with teachers, who provide feedback and guidance, enhancing learning without the pressure of grading. Additionally, the diary empowers students by giving them control over their entries, allowing them to keep reflections private or share them, fostering a sense of autonomy and ownership. This encourages honest and personal reflections, making the diary a valuable tool for personal development.</p>
-          </div>
-          
-          
-          
         </div>
         <div class="side-content">
-          <a href="/login" data-testid="loginyouknow" class="welcomeButtons">Already registered ?</a>
-          <a href="/Signup" class="welcomeButtons">Signup now !</a>
+          <button class="welcomeButtons"><a href="/login" data-testid="loginyouknow">Already registered ?</a></button>
+          <button class="welcomeButtons"><a href="/Signup">Signup now !</a></button>
         </div>
         
       </div>
